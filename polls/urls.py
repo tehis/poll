b666a12e65a55cef5ee227494ceb8ef26216601a
Ali <ali.khoshtinattt@gmail.com>
2019-05-14 12:06:51 +0430

    creating vote, results, detail views

--- polls/urls.py
+++ polls/urls.py
@@ -5,4 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
+    path('<int:questionId>/', views.detail, name = 'detail'),
+    path('<int:questionId>/results/', views.results, name = 'results'),
+    path('<int:questionId>/vote/', views.vote, name = 'vote')
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
+
+def detail(request, questionId):
+    return HttpResponse("You are looking at question %s." %questionId)
+
+def results(request, questionId):
+    response = "You are looking at the results of question %s."
+    return HttpResponse(response % questionId)
+
+def vote(request, questionId):
+    return HttpResponse("You are voting on question %s." %questionId)
 
 def index(request):
-    return  HttpResponse("Hello world!!! \n You are at the polls view")
+    latestQuetionList = Question.objects.order_by('ubDate')[:5]
+    output = ', '.join([q.questionText for q in latestQuetionList])
+    return  HttpResponse(output)
