b5749c7cfe83ade847efb6e552af86bc8ad4c09f
Ali <ali.khoshtinattt@gmail.com>
2019-05-14 13:02:49 +0430

    using Http404 in detail view

--- polls/views.py
+++ polls/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
 from django.template import loader
+from django.http import Http404
 
 
 def detail(request, questionId):
-    return HttpResponse("You are looking at question %s." %questionId)
+    try:
+        question = Question.objects.get(pk=questionId)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exists")
+    return render(request, 'polls/detail.html', {'question' : question})
 
 def results(request, questionId):
     response = "You are looking at the results of question %s."
