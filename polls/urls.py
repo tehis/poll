1d86f83e381d553d944474236873a9167b655a3e
Ali <ali.khoshtinattt@gmail.com>
2019-05-14 14:22:56 +0430

    Add detail templae and add another question

--- polls/urls.py
+++ polls/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 
 from . import views
 
-
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('<int:questionId>/', views.detail, name = 'detail'),
+    path('specifics/<int:questionId>/', views.detail, name = 'detail'),
     path('<int:questionId>/results/', views.results, name = 'results'),
     path('<int:questionId>/vote/', views.vote, name = 'vote')
 ]
