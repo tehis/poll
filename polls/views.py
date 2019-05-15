6c652f0592a20787ae68984355be05367080c2c3
Ali <ali.khoshtinattt@gmail.com>
2019-05-15 12:40:43 +0430

    Creating form and complete vote handling

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
+from .models import Question, Choice
 from django.template import loader
 from django.http import Http404
 
@@ -14,11 +16,24 @@ def detail(request, questionId):
     return render(request, 'polls/detail.html', {'question' : question})
 
 def results(request, questionId):
-    response = "You are looking at the results of question %s."
-    return HttpResponse(response % questionId)
+    question = get_object_or_404(Question, pk = questionId)
+    return render(request, 'polls/results.html', {'question' : question})
 
-def vote(request, questionId):
-    return HttpResponse("You are voting on question %s." %questionId)
+def vote(request, questionId) :
+    question = get_object_or_404(Question, pk = questionId)
+    try:
+        selectedchoice = question.choice_set.get(pk = request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return  render(request, 'polls/detail.html', {
+            'question' : question,
+            'errorMessage' : "You didnt selected any choice!",
+        })
+    else:
+        selectedchoice.votes += 1
+        selectedchoice.save()
+        return HttpResponseRedirect(reverse('polls:results',
+                                             args = (question.id, )))
+
 
 def index(request):
     latestQuetionList = Question.objects.order_by('pubDate')[:5]
